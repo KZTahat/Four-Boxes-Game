92a86d11c98895053c1709d8aa881ebcf601cdb8
KZTahat <kztahat96@gmail.com>
2022-02-25 17:45:33 +0200

    functionalization, adding Howler.js and changing IDs to classes

--- game.js
+++ game.js
@@ -1,49 +1,65 @@
-'use strict';
+"use strict";
+
 // setting enviromental variables
-
-function startGame() {
-    let startButton = document.getElementById('gameStartButton');
-    startButton.addEventListener('click', () => {
-        let reciptionContainer = document.getElementById('reciptionContainer');
-        reciptionContainer.style.display = 'none';
-    })
-}
-
-startGame();
-
 let score = 0;
 let lightBoxIndex = getRandomIndex(3, 0);
 
-let boxes = document.getElementsByClassName('boxes');
-let scoreSection = document.getElementById('scoreSection');
-appendScore();
+// Importing pages Containers
+let reciptionContainer = document.querySelector(".reciptionContainer");
+let gameContainer = document.querySelector(".gameContainer");
 
+// Start Game Function
+function startGame() {
+  let startGameSound = new Howl({
+    src: ["https://assets.codepen.io/21542/howler-push.mp3"],
+  });
+  let startButton = document.querySelector(".gameStartButton");
+  startButton.addEventListener("click", () => {
+    startGameSound.play();
+    reciptionContainer.style.display = "none";
+    gameContainer.style.display = "block";
+  });
+}
 
+function gamePage() {
+  let boxes = document.querySelectorAll(".boxes");
+  appendScore();
+  boxes[lightBoxIndex].style.backgroundColor = "red";
 
-boxes[lightBoxIndex].style.backgroundColor = 'red';
+  for (let i = 0; i < boxes.length; i++) {
+    boxes[i].addEventListener("click", () => {
+      if (boxes[i].style.backgroundColor == "red") {
+        boxes[i].style.backgroundColor = "gray";
+        lightBoxIndex = getRandomIndex(3, 0);
+        boxes[lightBoxIndex].style.backgroundColor = "red";
+        score++;
+        appendScore();
+      } else {
+        boxes[lightBoxIndex].style.backgroundColor = "gray";
+        lightBoxIndex = getRandomIndex(3, 0);
+        boxes[lightBoxIndex].style.backgroundColor = "red";
+        score--;
+        appendScore();
+      }
+    });
+  }
 
-for (let i = 0; i < boxes.length; i++) {
-    boxes[i].addEventListener('click', () => {
-        if (boxes[i].style.backgroundColor == 'red') {
-            boxes[i].style.backgroundColor = "gray";
-            lightBoxIndex = getRandomIndex(3, 0);
-            boxes[lightBoxIndex].style.backgroundColor = 'red';
-            score++;
-            appendScore();
-        } else {
-            boxes[lightBoxIndex].style.backgroundColor = "gray";
-            lightBoxIndex = getRandomIndex(3, 0);
-            boxes[lightBoxIndex].style.backgroundColor = 'red';
-            score--;
-            appendScore();
-        }
-    })
+  //   Handling Stop Button Click
+  let stopGameButton = document.querySelector(".stopGameButton");
+  stopGameButton.addEventListener("click", () => {
+    reciptionContainer.style.display = "block";
+    gameContainer.style.display = "none";
+  });
 }
 
 function getRandomIndex(max, min) {
-    return Math.floor(Math.random() * (max - min + 1) + min);
+  return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
 function appendScore() {
-    scoreSection.textContent = `Score: ${score}`;
+  let scoreSection = document.querySelector(".scoreSection");
+  scoreSection.textContent = `Score: ${score}`;
 }
+
+startGame();
+gamePage();
